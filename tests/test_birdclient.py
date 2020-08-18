9e5ff769d1c3875991bf6b8f15844ac3466e96fc
Nigel Kukard <nkukard@lbsd.net>
2020-08-18 12:22:21 +0000

    Removed regex class we don't need for our tests

--- tests/test_birdclient.py
+++ tests/test_birdclient.py
@@ -1,30 +1,26 @@
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2019-2020, AllWorldIT.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Tests for the Python BirdClient class."""
 
-import re
 from typing import List
 
 from birdclient import BirdClient
-
-
-class CustomPytestRegex:
-    """Assert that a given string meets some expectations."""
-
-    def __init__(self, pattern, flags=0):
-        """Inititalize object."""
-        self._regex = re.compile(pattern, flags)
-
-    def __eq__(self, actual):
-        """Check if the 'actual' string matches the regex."""
-        return bool(self._regex.match(actual))
-
-    def __repr__(self):
-        """Return our representation."""
-        return self._regex.pattern
-
-
-def since_field():
-    """Return our 'since' field match."""
-    return CustomPytestRegex(r"[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
 
 
 class TestBirdClient:
