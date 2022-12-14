db8ec3365db0798dc45926c7d621c0f3eaa4b0f7
Nigel Kukard <nkukard@lbsd.net>
2022-12-14 17:58:53 +0000

    Fixed RIP.02 quirk with Bird 2.0.11

--- birdclient/__init__.py
+++ birdclient/__init__.py
@@ -630,8 +630,11 @@ class BirdClient:
                     else:
                         raise BirdClientParseError("Unknown 'Kernel.source' attribute")
                 # Special case for RIP.tag
-                elif attrib == "RIP.tag":
+                elif attrib == "RIP.tag":  # noqa: SIM114
                     # This is a string (HEX)
+                    pass
+                # NK: Bird quirk with 2.0.11, this is supposed to be hidden
+                elif attrib == "RIP.02":
                     pass
                 # Finally if we don't understand the attribute
                 else:
